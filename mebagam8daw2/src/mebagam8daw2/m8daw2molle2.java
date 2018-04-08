e40686fe86c81724204bd191099b2109abf84253
merit <merit@LAPTOP-JGHHMPOI>
2018-04-08 20:52:01 +0200

    Versió 0.2 del projecte m8daw2molle2

--- mebagam8daw2/src/mebagam8daw2/m8daw2molle2.java
+++ mebagam8daw2/src/mebagam8daw2/m8daw2molle2.java
@@ -4,23 +4,33 @@ package m8daw2molle2;
 */
 import java.io.*;
 public class m8daw2molle2 {
-/**
-* @param args the command line arguments
-*/
-public static void main(String[] args) throws IOException {
-File f = new File("fitxer.html");
-try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
-bw.write("<html>");bw.newLine();
-bw.write(" <head>");bw.newLine();
-bw.write(" <title>");bw.newLine();
-bw.write(" Nova p&agrave;gina web");bw.newLine();
-bw.write(" </title>");bw.newLine();
-bw.write(" </head>");bw.newLine();
-bw.write(" <body>");bw.newLine();
-bw.write(" Nova p&agrave;gina web");bw.newLine();
-bw.write(" </body>");bw.newLine();
-bw.write("</html>");bw.newLine();
-bw.close();
-}
-}
+    /**
+    * @param args the command line arguments
+    */
+    public static void main(String[] args) throws IOException {
+        File f = new File("fitxer.html");
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))) {
+            bw.write("<html>");
+            bw.newLine();
+            bw.write(" <head>");
+            bw.newLine();
+            bw.write(" <title>");
+            bw.newLine();
+            bw.write(" Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write(" </title>");
+            bw.newLine();
+            bw.write(" </head>");
+            bw.newLine();
+            bw.write(" <body>");
+            bw.newLine();
+            bw.write(" Nova p&agrave;gina web");
+            bw.newLine();
+            bw.write(" </body>");
+            bw.newLine();
+            bw.write("</html>");
+            bw.newLine();
+            bw.close();
+        }
+    }
 }
